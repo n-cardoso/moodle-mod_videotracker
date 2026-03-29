30e823e0aee7b307845a6a2bcef9930dfd7b4196
NCard <48716929+n-cardoso@users.noreply.github.com>
2026-03-29 23:23:04 +0100

    Add course module viewed event for Video Tracker

---
+++ classes/event/course_module_viewed.php
@@ -0,0 +1,88 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Course module viewed event.
+ *
+ * @package     mod_videotracker
+ * @copyright   2026 LearnPlug
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace mod_videotracker\event;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Event fired when the Video Tracker activity is viewed.
+ */
+class course_module_viewed extends \core\event\course_module_viewed {
+    /**
+     * Initialise the event data.
+     *
+     * @return void
+     */
+    protected function init(): void {
+        $this->data['crud'] = 'r';
+        $this->data['edulevel'] = self::LEVEL_PARTICIPATING;
+        $this->data['objecttable'] = 'videotracker';
+    }
+
+    /**
+     * Return localised event name.
+     *
+     * @return string
+     */
+    public static function get_name(): string {
+        return get_string('eventcoursemoduleviewed', 'videotracker');
+    }
+
+    /**
+     * Return event description.
+     *
+     * @return string
+     */
+    public function get_description(): string {
+        return "The user with id '{$this->userid}' viewed the Video Tracker activity with course module id '{$this->contextinstanceid}'.";
+    }
+
+    /**
+     * Return the relevant URL.
+     *
+     * @return \moodle_url
+     */
+    public function get_url(): \moodle_url {
+        return new \moodle_url('/mod/videotracker/view.php', ['id' => $this->contextinstanceid]);
+    }
+
+    /**
+     * Return mapping information for backup and restore.
+     *
+     * @return array
+     */
+    public static function get_objectid_mapping(): array {
+        return ['db' => 'videotracker', 'restore' => 'videotracker'];
+    }
+
+    /**
+     * Return other mapping information for backup and restore.
+     *
+     * @return array|bool
+     */
+    public static function get_other_mapping() {
+        return false;
+    }
+}
